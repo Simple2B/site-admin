1fff04d6fb9e0c286bcc1f2ca75b5916b2bbaf86
Kostiantyn Stoliarskyi <saintkos117@gmail.com>
2023-07-27 11:18:53 +0300

    add tests

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -1,6 +1,5 @@
-from flask import Response, current_app as app
-from flask.testing import FlaskClient, FlaskCliRunner
-from click.testing import Result
+from flask import Response
+from flask.testing import FlaskClient
 from app.common import models as m
 from tests.utils import login
 from app.database import db
@@ -28,9 +27,9 @@ def test_CRUD_questions(client: FlaskClient):
     assert question.correct_answer_mark == 1
     response: Response = client.post(
         "/quiz/save",
-        data=dict(
+        json=dict(
             id=question.id,
-            uid=question.uid,
+            uuid=question.uid,
             text="Test question!!!",
             correct_answer_mark="2",
             variant_1="Variant 1",
@@ -45,11 +44,10 @@ def test_CRUD_questions(client: FlaskClient):
     assert edited_question
     assert edited_question.text == "Test question!!!"
     assert edited_question.correct_answer_mark == 2
-
-
-# def test_delete_user(populate: FlaskClient):
-#     login(populate)
-#     uc = db.session.query(m.SuperUser).count()
-#     response = populate.delete("/user/delete/1")
-#     assert db.session.query(m.SuperUser).count() < uc
-#     assert response.status_code == 200
+    response: Response = client.delete(
+        "/quiz/delete/1",
+        follow_redirects=True,
+    )
+    assert response
+    assert response.status_code == 200
+    assert not db.session.query(m.Question).first()
