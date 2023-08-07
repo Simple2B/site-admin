e4761350b83cc8bdd160783c66ec8f176351d8a7
AlexAzalor <xy55xy.azalor@gmail.com>
2023-08-07 17:34:11 +0300

    elements checking, delete stack test

--- src/stack.ts
+++ src/stack.ts
@@ -3,11 +3,16 @@ import type { ModalOptions, ModalInterface } from 'flowbite';
 
 export const stack = () => {
   const form = document.getElementById('add-stack-form') as HTMLFormElement;
-  const name = document.getElementById('add-stack-form-input') as HTMLInputElement
-  const stacksDiv = document.getElementById('add-stack-form-stacks')
+  const inputElement = document.getElementById('add-stack-form-input') as HTMLInputElement;
+  const stacksDiv = document.getElementById('add-stack-form-stacks');
   const scrfInput: HTMLInputElement =
     document.querySelector('#csrf_token');
-  const oldSpansName = Array.from(stacksDiv.querySelectorAll('span')).map((span) => span.textContent)
+
+  let oldSpansName: string[];
+
+  if (stacksDiv) {
+    oldSpansName = Array.from(stacksDiv.querySelectorAll('span')).map((span) => span.textContent);
+  }
 
   const listElementsCases = document.getElementById('listOfCases');
   const $deleteStackModal: HTMLElement =
@@ -21,10 +26,14 @@ export const stack = () => {
     onToggle: () => { },
   };
 
-  const stackModalWarning: ModalInterface = new Modal(
-    $deleteStackModal,
-    modalOptions
-  )
+  let stackModalWarning: ModalInterface;
+
+  if ($deleteStackModal) {
+    stackModalWarning = new Modal(
+      $deleteStackModal,
+      modalOptions
+    )
+  }
 
   const stackModalCloseBtn = document.querySelector('#modalStackCloseButtonError');
   if (stackModalCloseBtn) {
@@ -33,93 +42,105 @@ export const stack = () => {
     });
   }
 
-  document.querySelectorAll('[id^="button-"]').forEach(button => {
-    const stackName = button.id.replace("button-", "");
-    button.addEventListener("click", async () => {
+  // This selects elements where the id attribute starts with the string "button-"
+  const listOfStacks = document.querySelectorAll('[id^="button-"]');
 
-      const formData = new FormData();
-      formData.append('stacks', stackName);
+  if (listOfStacks) {
+    listOfStacks.forEach(button => {
+      const stackName = button.id.replace("button-", "");
+      button.addEventListener("click", async () => {
 
-      const response = await fetch(`/stack/delete`, {
-        method: 'DELETE',
-        body: formData,
-      });
+        const formData = new FormData();
+        formData.append('stacks', stackName);
 
-      if (response.status == 422) {
-        const stringList = await response.json();
-
-        // Clear existing content
-        listElementsCases.innerHTML = '';
-
-        // Create a <span> element for each string and append it to the container
-        stringList.forEach((item: any) => {
-          const span = document.createElement('span');
-          const lineBreak = document.createElement('br');
-          span.textContent = item;
-          span.setAttribute("class", "font-semibold text-xl");
-          listElementsCases.appendChild(span);
-          listElementsCases.appendChild(lineBreak);
+        const response = await fetch(`/stack/delete`, {
+          method: 'DELETE',
+          body: formData,
         });
 
-        stackModalWarning.show();
-      }
+        if (response.status === 422 && listElementsCases) {
+          const stringList = await response.json();
 
-      if (response.status == 200) {
-        console.log('200');
-        location.reload();
-      }
+          // Clear existing content
+          listElementsCases.innerHTML = '';
+
+          // Create a <span> element for each string and append it to the container
+          stringList.forEach((item: string) => {
+
+            const span = document.createElement('span');
+            const lineBreak = document.createElement('br');
+
+            span.textContent = item;
+            span.setAttribute("class", "font-semibold text-xl");
+            listElementsCases.appendChild(span);
+            listElementsCases.appendChild(lineBreak);
+          });
+
+          stackModalWarning.show();
+        }
+
+        if (response.status == 200) {
+          location.reload();
+        }
+      });
     });
-  });
+  }
 
-  form.onkeydown = function (e) {
-    if (e.keyCode == 13 && name.value && stacksDiv) {
-      e.preventDefault();
+  if (form) {
+    form.onkeydown = function (e) {
+      if (e.key === 'Enter' && inputElement.value && stacksDiv) {
+        e.preventDefault();
 
-      const newStack = document.createElement('span');
+        const newStack = document.createElement('span');
 
-      newStack.textContent = name.value
-      newStack.setAttribute("class", "bg-indigo-200 text-indigo-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400")
+        newStack.textContent = inputElement.value
+        newStack.setAttribute("class", "bg-indigo-200 text-indigo-800 text-lg font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-indigo-400 border border-indigo-400")
 
-      const newSpansName = Array.from(stacksDiv.querySelectorAll('span')).map((span) => span.textContent);
+        const newSpansName = Array.from(stacksDiv.querySelectorAll('span')).map((span) => span.textContent);
 
-      if (!oldSpansName.includes(name.value) && !newSpansName.includes(name.value)) {
-        stacksDiv.appendChild(newStack);
+        if (!oldSpansName.includes(inputElement.value) && !newSpansName.includes(inputElement.value)) {
+          stacksDiv.appendChild(newStack);
+        }
+
+        inputElement.value = ""
       }
-
-      name.value = ""
     }
   }
 
   async function handleSubmit(event: Event) {
     event.preventDefault();
 
-    const newStacks = Array.from(
-      stacksDiv.querySelectorAll('span')
-    ).map((span) => span.textContent
-    ).filter((stack) => !oldSpansName.includes(stack))
+    if (stacksDiv) {
+      const newStacks = Array.from(
+        stacksDiv.querySelectorAll('span')
+      ).map((span) => span.textContent
+      ).filter((stack) => !oldSpansName.includes(stack))
 
-    if (name && name.value && !newStacks.includes(name.value)) {
-      newStacks.push(name.value)
-    }
+      if (inputElement && inputElement.value && !newStacks.includes(inputElement.value)) {
+        newStacks.push(inputElement.value)
+      }
 
-    const fetchStr = newStacks.join(',');
+      const fetchStr = newStacks.join(',');
 
-    if (fetchStr) {
-      const formData = new FormData();
-      formData.append('stacks', fetchStr);
-      formData.append("csrf_token", scrfInput ? scrfInput.value : '',)
+      if (fetchStr) {
+        const formData = new FormData();
+        formData.append('stacks', fetchStr);
+        formData.append("csrf_token", scrfInput ? scrfInput.value : '',)
 
-      const response = await fetch(`/stack/create`, {
-        method: 'POST',
-        body: formData,
-      });
+        const response = await fetch(`/stack/create`, {
+          method: 'POST',
+          body: formData,
+        });
 
-      if (response.status == 200) {
-        location.reload();
-        name.value = ""
+        if (response.status == 200) {
+          location.reload();
+          inputElement.value = ""
+        }
       }
     }
   }
 
-  form.addEventListener('submit', handleSubmit);
+  if (form) {
+    form.addEventListener('submit', handleSubmit);
+  }
 }
